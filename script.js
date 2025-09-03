7ba9c28a2cb59716a7ea903ea055bfa59b8f5919
gui dixava <141260850+AguinaldoRemes@users.noreply.github.com>
2025-09-03 14:26:58 -0300

    34235

--- script.js
+++ script.js
@@ -6,3 +6,42 @@ const botoes = document.querySelectorAll('parametro-senha-botao');
 const forcaSenha = document.querySelector('.forca');
 botoes[0].onclick = diminiuTamanho;
 botoes[1].onclick = aumentaTamanho;
+ function diminiuTamanho(){
+    if (tamanhoSenha> 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    gerasenha();
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    gerasenha();
+}
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '123456789';
+const simbolos = '!@#%*?';
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){  
+        alfabeto = alfabeto + letrasMaiusculas;
+        }
+}
+if (checkbox[1].checked){  
+        alfabeto = alfabeto + letrasMinusculas;
+        }
+        if (checkbox[2].checked){  
+        alfabeto = alfabeto + numeros;
+        }
+        if (checkbox[3].checked){  
+        alfabeto = alfabeto + simbolos;
+        }
+let senha = '';
+for (let) i = 0; i < tamanhoSenha; i++){
+
+}
+        
